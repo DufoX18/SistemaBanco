95f358cddb7faf463648f93e15a763905c12d0b7
DufoX18 <carodri323@gmail.com>
2023-08-23 10:22:14 -0600

    Clase AministracionListaClientes
    Se crean los metodos de la clase

---
+++ src/examenii/AdministracionListaTickets.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examenii;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Student
+ */
+public class AdministracionListaTickets extends AdministracionTickets {
+ HashMap<Integer, Tipo> ticket = new HashMap();
+
+    public HashMap<Integer, Tipo> getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(HashMap<Integer, Tipo> ticket) {
+        this.ticket = ticket;
+    }
+
+    public AdministracionListaTickets(Tipo tipo, int numero) {
+        super(tipo, numero);
+    }
+ 
+    public boolean eliminar(int t, Tipo tipo) {
+
+        if (this.ticket.isEmpty()) {
+            ticket.remove(t, tipo);
+            return true;
+        }
+        return false;
+
+    }
+
+    public HashMap<Integer, Tipo> buscar(Tipo tipo) {
+        if (ticket.containsValue(tipo)) {
+            return ticket;
+        }
+        return null;
+    }
+
+    public boolean insertar(int t, Tipo tipo) {
+        if (this.ticket == null) {
+            ticket.put(t, tipo);
+            return true;
+        }
+        return false;
+
+    }
+}
--- src/examenii/AdministracionTickets.java
+++ src/examenii/AdministracionTickets.java
@@ -4,11 +4,42 @@
  */
 package examenii;
 
+import java.util.HashMap;
+
 /**
  *
  * @author Student
  */
 public class AdministracionTickets {
-   
-    
+
+    private Tipo tipo;
+    private int numero;
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public AdministracionTickets(Tipo tipo, int numero) {
+        this.tipo = tipo;
+        this.numero = numero;
+
+    }
+
+    @Override
+    public String toString() {
+        return "AdministracionTickets{" + "tipo=" + tipo + '}';
+    }
+
 }
